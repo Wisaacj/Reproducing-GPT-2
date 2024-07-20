5312b8bbd9fe9f3fe519a28d8eb8db5599b88c85
Wisaacj <wisaacj@live.co.uk>
2024-07-20 14:42:50 +0100

    Coded GPT-2 from scratch. You can load GPT-2 model weights from a HuggingFace checkpoint and run inference using a naive sampling loop

---
+++ train_gpt2.py
@@ -0,0 +1,266 @@
+import tiktoken
+import math
+import torch
+import torch.nn as nn
+
+from dataclasses import dataclass
+from torch.nn import functional as F
+
+
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # Key, query, and value projections for all heads, but in a batch.
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        # Output projection.
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        # Regularisation
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        # Not really a 'bias', more of a mask, but following teh OpenAI/HF naming though
+        self.register_buffer("bias", torch.tril(torch.ones(
+            config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
+
+    def forward(self, x):
+        # The input is a tensor of shape (batch_size, seq_len, n_embd)
+        # The output is a tensor of shape (batch_size, seq_len, n_embd)
+        # The attention mask is a tensor of shape (1, 1, seq_len, seq_len)
+        # The attention mask is a lower triangular matrix of ones.
+        # The attention mask is broadcasted to the batch size.
+        # The attention mask is broadcasted to the number of heads
+
+        B, T, C = x.size()  # Batch size, sequence length, embedding dimensionality (n_embd)
+        # Calculate query, key, values for all heads in one batch and move head forward
+        # to be the batch dim. I.e., nh is a "batch dimension" meaning that PyTorch treats
+        # B & nh as batches and it applies all the operations on both the batches and
+        # heads in parallel. Notice that this is not *just* attention, this is multi-head attention.
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        # nh is the "number of heads", hs is "head size", and C (number of channels) = nh * hs
+        # e.g., in GPT-2 (124M), n_head=12, hs=64, so nh*hs=768 channels in the Transformer.
+        k = k.view(B, T, self.n_head, C //
+                   self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C //
+                   self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C //
+                   self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        # Attention (materialises the large (T, T) attention matrix for all the queries and keys)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        # This autoregressive mask makes sure that the tokens only attend to tokens before them
+        # and never to tokens in the future.
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        # Recall that attention multipled by the values is basically a way of doing a weighted
+        # sum of the values (the attention scores sum to 1).
+        y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        # Output projection
+        y = self.c_proj(y)
+        return y
+
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        # The MLP is a two-layer feedforward neural network with a GELU activation funciton.
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        # Guassian Error Linear Unit (GELU) is a non-linear activation function that is a
+        # smooth version of the ReLU. We are using the tanh approximation because the original
+        # GPT-2 implementation used it. Back then, calculating the GELU was expensive.
+        self.gelu = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+
+class Block(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        # Attention is a pooling function, a weighted aggregration function, a reduce function.
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        # Whereas, a MLP happens to every token individually. The MLP is a map function.
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        # The forward pass of the block is the forward pass of the two sublayers,
+        # each with a residual skip connection.
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
+@dataclass
+class GPTConfig:
+    block_size: int = 1024  # Max sequence length
+    # Number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
+    vocab_size: int = 50257
+    n_layer: int = 12  # Number of layers
+    n_head: int = 12  # Number of heads
+    n_embd: int = 768  # Embedding dimension
+
+
+class GPT(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            # Weights of the token embeddings. Recall that nn.Embedding is really a
+            # wrapper around a Tensor of shape (vocab_size, n_embd).
+            wte=nn.Embedding(config.vocab_size, config.n_embd),
+            # Weights of the position embeddings
+            wpe=nn.Embedding(config.block_size, config.n_embd),
+            # h for hidden. Each block is the grey section of the decoder diagram in
+            # Attention is All You Need (Vaswani et al., 2017).
+            h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            # Finaly layer normalisation
+            ln_f=nn.LayerNorm(config.n_embd),
+        ))
+
+        # Finally, the langauge model head projects from n_embd to vocab_size with no bias.
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+    def forward(self, idx):
+        # The input to the foward pass is a sequence of token indices.
+        # idx is of shape (B, T)
+        B, T = idx.size()
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {
+            T}, block size is only {self.config.block_size}"
+        # Forward the token and positional embeddings
+        pos = torch.arange(0, T, dtype=torch.long,
+                           device=idx.device)  # Shape (T,)
+        # Position embeddings of shape (T, n_embd)
+        pos_emb = self.transformer.wpe(pos)
+        # Token embeddings of shape (B, T, n_embd)
+        tok_emb = self.transformer.wte(idx)
+        x = tok_emb + pos_emb
+        # Forward the blocks of the transformer
+        for block in self.transformer.h:
+            x = block(x)
+        # Forward the final layer norm and the classifier
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x)  # (B, T, vocab_size)
+        return logits
+
+    @classmethod
+    def from_pretrained(cls, model_type: str):
+        """
+        Loads pretrained GPT-2 model weights from huggingface
+        """
+        assert model_type in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
+        from transformers import GPT2LMHeadModel
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head, and n_embd are determined from model_type
+        config_args = {
+            # 124M params
+            "gpt2": dict(n_layer=12, n_head=12, n_embd=768),
+            # 350M params
+            "gpt2-medium": dict(n_layer=24, n_head=16, n_embd=1024),
+            # 774M params
+            "gpt2-large": dict(n_layer=36, n_head=20, n_embd=1280),
+            # 1558M params
+            "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600),
+        }[model_type]
+        # Always 50257 for GPT model checkpoints
+        config_args['vocab_size'] = 50257
+        # Always 1024 for GPT model checkpoints
+        config_args['block_size'] = 1024
+        # Create a from-scratch initialised minGPT model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith(
+            '.attn.bias')]  # Discard this mask
+
+        # Init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # Copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(
+            ".attn.masked_bias")]
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.bias")]
+        tranposed = ['attn.c_attn.weight', 'attn.c_proj.weight',
+                     'mlp.c_fc.weight', 'mlp.c_proj.weight']
+
+        # Basically, the OpenAI checkpoints ue a "Conv1D" module, but we only want to use a vanilla module.
+        # This means we have to tranpose these weights when we import them.
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {
+            len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            print(f"copying weights: {k}")
+            if any(k.endswith(w) for w in tranposed):
+                # Special treatment for the Conv1D weights we need to tranpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # Vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+
+
+num_return_sequences = 5
+max_length = 30
+
+model = GPT.from_pretrained("gpt2")
+print("didn't crash yay")
+
+model.eval()
+model.to('cuda')
+
+# Prefix tokens (which are subsequently fed into the model as a prompt)
+enc = tiktoken.get_encoding("gpt2")
+tokens = enc.encode("Hello, I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long)  # (8,)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)  # (5, 8)
+x = tokens.to('cuda')  # x is idx
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+# Generate! Right now x is (B, T) where B = 5, T = 8
+while x.size(1) < max_length:
+    # Forward the model to get the logits. Wrapping this block in a torch.no_grad() context
+    # manager indicates to PyTorch that we'll not be calling .backward() on any of the code.
+    # This means PyTorch doesn't need to cache any of the intermediate Tensors and, subsequently,
+    # that the operations within don't build up the computational graph. This makes the
+    # inference faster and reduces the memory consumption.
+    with torch.no_grad():
+        logits = model(x)  # (B, T, vocab_size)
+        # Take the logtis at the last position
+        logits = logits[:, -1, :]  # (B, vocab_size)
+        # Get the probabilities
+        prob = F.softmax(logits, dim=-1)
+        # Do top-k sampling of 50 (huggingface pipeline default). This ensures that we never
+        # sample very unlikely tokens, but rather always sample from the top-k most likely tokens.
+        # topk_probs here becomes (5, 50), topk_indices becomes (5, 50)
+        topk_probs, topk_indices = prob.topk(50, dim=-1)
+        # Select a token from the top-k probabilities
+        ix = torch.multinomial(topk_probs, num_samples=1)  # (B, 1)
+        # Gather the corresponding token indices
+        xcol = topk_indices.gather(dim=-1, index=ix)  # (B, 1)
+        # Append to the sequence
+        x = torch.cat((x, xcol), dim=1)
+
+# Print the generated text
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
