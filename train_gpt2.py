74aa671350c9caf86f6d3149f9d344c2ca8c4c23
Wisaacj <wisaacj@live.co.uk>
2024-07-22 12:41:22 +0100

    Learning rate schedule; introducing fused AdamW; changing the weight decay for >2D tensors

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,3 +1,4 @@
+import inspect
 import time
 import sys
 import tiktoken
@@ -252,6 +253,33 @@ class GPT(nn.Module):
 
         return model
 
+    def configure_optimizers(self, weight_decay, learning_rate, device):
+        # Start with all the candidate parameters (that required grad)
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        # Create optim groups. Any parameter that is 2D will be weight decayed, otherwise it won't.
+        # I.e., all weight tensors in matmuls + embeddings will decay, all biases and layernorms won't.
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {"params": decay_params, "weight_decay": weight_decay},
+            {"params": nodecay_params, "weight_decay": 0.0},
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(
+            f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(
+            f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        # Create AdamW optimizer and use the fused version if it is available.
+        fused_available = 'fused' in inspect.signature(
+            torch.optim.AdamW).parameters
+        use_fused = fused_available and 'cuda' in device
+        print(f"using fused AdamW: {use_fused}")
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(
+            0.9, 0.95), eps=1e-8, fused=use_fused)
+        return optimizer
+
 
 class DataLoaderLite:
 
@@ -306,15 +334,40 @@ train_loader = DataLoaderLite(B=8, T=1024)
 # Enable TF32 for faster training
 torch.set_float32_matmul_precision('high')
 
-# Get logits
+# Using vocab_size=50304 is a code performance optimisation (4% improvement). GPT-2 has a vocabulary size of 50257.
 model = GPT(GPTConfig(vocab_size=50304))
 model.to(device)
 model = torch.compile(model)
 
+max_lr = 6e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps = 50
+
+
+def get_lr(it):
+    """Learning rate schedule"""
+    # 1) Linear warmup for warmup_steps
+    if it < warmup_steps:
+        # Using (it + 1) because a learning rate of zero is not useful.
+        return max_lr * (it+1) / warmup_steps
+    # 2) If it > max_steps, return min learning rate
+    if it > max_steps:
+        return min_lr
+    # 3) In between, use cosine decay down to min learning rate
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    return min_lr + coeff * (max_lr - min_lr)
+
+
 # AdamW is a bug fix for Adam. It's the same as Adam, but with a different weight decay.
-optimizer = torch.optim.AdamW(
-    model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
-for i in range(50):
+# optimizer = torch.optim.AdamW(
+#     model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
+optimizer = model.configure_optimizers(
+    weight_decay=0.1, learning_rate=6e-4, device=device)
+
+for step in range(max_steps):
     t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
@@ -327,6 +380,10 @@ for i in range(50):
     # Gradient clipping is a technique to prevent exploding gradients in very deep networks.
     # A very bad batch of data can cause the gradients to explode and the model to fail to train.
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+    # Determine and set the learning rate for this iteration.
+    lr = get_lr(step)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
     # Calling .step() updates the parameters based on the gradients.
     optimizer.step()
     # Wait for the GPU to finish all work scheduled to run.
@@ -334,7 +391,7 @@ for i in range(50):
     t1 = time.time()
     dt = (t1 - t0)*1000  # Time difference in milliseconds
     tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
-    print(f"""step {i} | loss: {loss.item():.6f} | norm: {norm:.4f} | dt: {
+    print(f"""step {step:4d} | loss: {loss.item():.6f} | lr: {lr:.4f} | norm: {norm:.4f} | dt: {
           dt:.2f}ms | tok/sec: {tokens_per_sec:.2f}""".replace("\n", ""))
 
 sys.exit(0)
